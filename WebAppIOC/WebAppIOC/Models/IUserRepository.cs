b1ba2450189de2c5909917e13cb97a105605bb47
kevonthebus <kmatth.007@gmail.com>
2018-09-12 13:53:23 -0400

    #added model and repo

---
+++ WebAppIOC/WebAppIOC/Models/IUserRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppIOC.Models
+{
+    public interface IUserRepository
+    {
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        public List<User> GetAll()
+        {
+            return new List<User>()
+        {
+            new User()
+            {
+                FirstName = "Ash",
+                LastName = "Ketchum",
+                DateOfBirth = new DateTime(1997, 12, 30),
+                Username = "ichooseyou"
+            },
+            new User()
+            {
+                FirstName = "Brock",
+                LastName = "Harrison",
+                DateOfBirth = new DateTime(1992, 3,31),
+                Username = "rockrulez"
+            },
+            new User()
+            {
+                FirstName = "Misty",
+                LastName = "",
+                DateOfBirth = new DateTime(1999, 5,4),
+                Username = "ihearttogepi"
+            }
+        };
+        }
+    }
+}
